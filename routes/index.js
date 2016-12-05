30a5f415c587b3ecc158359dfff884330f1f8c77
Sthefany Floriano <Sthefany Floriano>
2016-12-05 17:25:33 -0500

    probando

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var html_dir = './public/admin/';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.sendfile(html_dir + 'index.html');
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var html_dir = './public/';
+var html_dir = './public/user/';
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-	res.sendfile(html_dir + 'votar.html');
+	res.sendfile(html_dir + 'primera-votacion.html');
 });
-
 module.exports = router;
 
