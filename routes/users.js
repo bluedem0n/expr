8d663316b5fcbc45e937980370ee6b7142443d22
Sthefany Floriano <Sthefany Floriano>
2016-12-05 17:03:22 -0500

    route for users

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
-
+var html_dir = './public/';
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+	res.sendfile(html_dir + 'index.html');
 });
 
 module.exports = router;
+
