1594e38d63031e8f4f0c83b66ad1329c64210a64
Sthefany Floriano <Sthefany Floriano>
2016-12-05 17:07:13 -0500

    votar

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var html_dir = './public/';
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-	res.sendfile(html_dir + 'index.html');
+	res.sendfile(html_dir + 'votar.html');
 });
 
 module.exports = router;
